87039dd438d8cb9d0a05968e2e221916b4923a29
zbyna64 <108943110+zbyna64@users.noreply.github.com>
2023-04-12 20:31:42 +0200

    Test(): unit service test

---
+++ fh/src/test/java/com/fh/fh/services/ItemServiceTest.java
@@ -0,0 +1,46 @@
+package com.fh.fh.services;
+
+import com.fh.fh.models.Item;
+import com.fh.fh.models.ItemRequestDTO;
+import com.fh.fh.models.ItemResponseDTO;
+import com.fh.fh.models.User;
+import com.fh.fh.repositories.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemServiceTest {
+  @Mock
+  private UserService userService;
+  @Mock
+  private ItemRepository itemRepository;
+  @InjectMocks
+  private ItemService itemService;
+
+  @Test
+  public void testCreateItemWithUser() {
+
+    ItemRequestDTO requestDTO = new ItemRequestDTO("item", "test purpose", 1000.0, 2000.0);
+    Authentication authentication = mock(Authentication.class);
+    User testUser = new User("zbyna", "1234");
+    Item item = new Item("item", "test purpose", 1000.0, 2000.0 , testUser);
+
+    Mockito.when(authentication.getName()).thenReturn("zbyna");
+    Mockito.when(userService.findByUsername(any(String.class))).thenReturn(testUser);
+    Mockito.when(itemRepository.save(any(Item.class))).thenReturn(item);
+
+    ItemResponseDTO response = itemService.createItem(requestDTO, authentication);
+
+    assertEquals("item", response.getName());
+    verify(itemRepository, times(1)).save(any());
+  }
+}
